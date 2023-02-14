11c8e8867e43ee1ab8827c7fb270cb5bd83ca9cd
DanilG73 <123488618+DanilG73@users.noreply.github.com>
2023-02-14 20:39:29 +0200

    Add files via upload

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            //int[] omas = новый int[6];
+            //int i;
+            //для (i = 0; iДлина; i++)
+            //{
+            // Консоль.Написать($"omas[{i}]=", i);
+            // omas[i] = int.Синтаксический анализ(консоль.Строка чтения());
+            //}
+            //для (i = 0; iДлина; i++)
+            //{
+            // Консоль.Написать(" " + omas[i]);
+            //}
+            // Задание 2
+            //int[] omas = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //int i;
+            //для(i = 0; i < омас.Длина; i++)
+            //{
+            // Консоль.Строка записи($"omas=[{i}]={omas[i]}");
+            //}
+            // Задание 3 
+            //int[] intArr;
+            //intArr = новый int[6];
+            //foreach(int i в intArr)
+            //{
+            // Консоль.Написать(" " + intArr[i]);
+            // intArr[i] = i+4;
+            //}
+            // Задание 4 
+            //Консоль.Write("Введите размерность массивов (от 5 до 20) ");
+            //int n = int.Синтаксический анализ(Console.Строка чтения());
+            //Случайный r = новый случайный(); 
+            //int[] a = новый int[n];
+            //foreach (входной элемент в a)
+            //{
+            // если (элемент < 0)
+            //    {
+            // a[n] = r.Следующий(-20, 20); 
+            // Консоль.Написать("{0,4}", a[n]);
+            //    }
+            //}
+            Console.ReadKey();
+
+        }
+    }
+}
